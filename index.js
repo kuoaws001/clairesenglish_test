e36438689a519ac6bbe30f6b0000cfea077ee8af
chris.kuo <126221813+beyondcars@users.noreply.github.com>
2024-04-08 16:37:08 +0800

    nodemailer sendEmail

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const pageRouter = require('./routes/page');
 const apiRouter = require('./routes/api');
-const emailRouter = require('./routes/email')
+const emailRouter = require('./routes/email');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,8 +13,8 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', pageRouter)
-app.use('/api', apiRouter)
-app.use('/emails', emailRouter)
+app.use('/', pageRouter);
+app.use('/api', apiRouter);
+app.use('/emails', emailRouter);
 
 app.listen(port)
--- routes/email.js
+++ routes/email.js
@@ -1,18 +1,40 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const router = express.Router();
 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'kuolearnaws@gmail.com',
+        pass: process.env.SMPT_GMAIL_PASS,
+    },
+});
+
 router.post('/', (req, res, next) => {
     const { to, subject, body } = req.body;
+    // TODO : add server side validation
 
-    res.send({
+    const mailOptions = {
+        from: 'kuolearnaws@gmail.com',
         to,
         subject,
-        body
+        text: body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.status(400).send({ error: `Error in sending email ${error}` })
+        } else {
+            res.send({ success: `Email sent: ${info.response}` })
+        }
     })
 })
 
 module.exports = router;
 
 
-// TODO : add server side validation
